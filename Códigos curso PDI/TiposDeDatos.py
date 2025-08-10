a47ea1b89c7722c47093cc3354d9197cf55eab9f
cherrycita-dev <pereznancyesmeralda@gmail.com>
2025-08-09 19:18:43 -0600

    Add files via upload

---
+++ Códigos curso PDI/TiposDeDatos.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  9 19:08:00 2025
+
+@author: Nancy
+"""
+
+print (-5)
+
+#Flotante
+# Todos los números reales de -infinito a +infinito 
+
+print (0.4)
+
+#Booleanos
+# Dos unico valores, verdadero o falso (True or False)
+
+print(True)
+print(False)
+
+#Cadena
+#Se define como cualquier texto entre comillas
+
+print("Hola 1234")
+
+# Poner comillas
+
+print('el "expreso"')
+
+
+# Listas
+# Se define como una secuencia mutable
+
+print ([1,2,3,"q","a","s","d",(1,2,3,'a',5,'r',[1,2,3,"q","a","s","d"])])
+
+#Tupla
+# Se define como una secuencia inmutable
+print((1,2,3,'a',5,'r',[1,2,3,"q","a","s","d"]))
+
+
+#Diccionario
+# Tipo de dato que asocia claves con valores {1: 'algo'}, se pueden agregar tuplas y listas en este 
+print({1: 'algo'})
+
