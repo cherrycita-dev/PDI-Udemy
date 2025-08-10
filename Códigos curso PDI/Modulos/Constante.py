d2f1fd7fecf932be79599edc03bcff9f45f4962a
cherrycita-dev <pereznancyesmeralda@gmail.com>
2025-08-10 02:58:39 -0600

    Add files via upload

---
+++ Códigos curso PDI/Modulos/Constante.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 10 02:08:22 2025
+
+@author: Nancy
+"""
+
+# Modulo donde almacenamos constantes
+
+pi= 3.1416
+contraseña= "123456"
+direccion= "www.google.com"
---
+++ Códigos curso PDI/Variables.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  9 19:54:13 2025
+
+@author: Nancy
+"""
+
+# Variables y constantes
+# Nombres de las variables pueden contener: letras, num, _
+# Nombre puede comenzar con letras, _, pero no con num
+# Puede diferencias entre mayusculas y minusculas
+# Nombre descriptivo, usar mayusculas nombreAlumnos
+# Ejemplo
+
+valor1=5
+valor2=8
+
+# print(valor1)
+# print(valor2)
+# print(valor1,valor2)
+# print("La suma de estas dos variables es:", valor2 + valor1)
+
+
+# cadenaSuma= "La suma de estas dos variables es:"
+# suma= valor1+valor2
+
+# print(cadenaSuma, suma)
+
+# cadenaResta="La resta de estos dos valores es:"
+# resta= valor1-valor2
+# print(cadenaResta, resta)
+
+
+
+# Usando cadenas
+
+# nombrePersona= "Nancy"
+# apellidoPersona= "Pérez"
+
+# print("Mi nombre es:", nombrePersona)
+# print("Mi apellido es ", apellidoPersona)
+# Concatenación
+# nombreCompleto= nombrePersona + ' ' + apellidoPersona
+# print("Mi nombre completo es", nombreCompleto)
+
+# Con diccionario de datos, con indices
+# print("Mi nombre es {0} y mi apellido es {1}".format (nombrePersona, apellidoPersona))
+
+# # Otra manera 
+# print("Mi nombre es {nombre} y mi apellido es {apellido}".format (nombre= nombrePersona, apellido =apellidoPersona))
+
+
+
+# # Función input, ingresar desde teclado
+
+
+# nombreCompleto = input ("Ingrese su nombre completo")
+# print("Su nombre completo es:", nombreCompleto)
+
+
+# valor1= int(input("Ingresa el primer valor: "))
+
+# valor1= input("Ingresa el primer valor: ")
+# valor2= input("Ingresa el segundo valor: ")
+
+# suma= int(valor1) + int(valor2)
+# # Da 12 ya que lo lee como caracter, no como entero
+# print("La suma es igual a ", suma)
+
+
+# nombrePersona= input("Ingrese su nombre: ")
+# edad = int (input("Ingrese su edad: "))
+
+# print("Su nombre es {0} y su edad {1}".format(nombrePersona, edad))
+
+
+
+# Otra forma de hacerlo
+from Modulos.Constante import *
+
+print(pi + pi)
+print(int (contraseña)+ pi)
+
+
+
